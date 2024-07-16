6a4b07b4ced3100ced2c745cd73eecdbf0d11d0a
Guyllaume Rousseau <32419088+GRV96@users.noreply.github.com>
2024-07-16 13:10:00 -0400

    Day 1 (#1)
    Puzzle 1: 74
    Puzzle 2: 1795

---
+++ AdventOfCode2015/Day1/Day1.cpp
@@ -0,0 +1,59 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#define PARENTHESIS_OPEN '('
+#define PARENTHESIS_COLSED ')'
+
+void parseInstructionLine(
+    const std::string& const pInstructionLine, int* const pAscension, int* const pBasementInstructionIndex)
+{
+    *pBasementInstructionIndex = -1;
+    *pAscension = 0;
+
+    int nbInstructions = pInstructionLine.length();
+    for (int i = 0; i < nbInstructions; i++)
+    {
+        char instruction = pInstructionLine[i];
+
+        if (instruction == PARENTHESIS_OPEN)
+        {
+            (*pAscension)++;
+        }
+        else if (instruction == PARENTHESIS_COLSED)
+        {
+            (*pAscension)--;
+        }
+
+        if (*pBasementInstructionIndex < 0 && *pAscension == -1)
+        {
+            *pBasementInstructionIndex = i;
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    char* intputPath = argv[1];
+    std::ifstream inputFile(intputPath);
+    std::string instructionLine;
+    int ascension = 0;
+
+    // Position of the first instruction to enter the basement
+    int basementInstructionPosition = -1;
+
+    while (inputFile.good())
+    {
+        int basementPositionIndex;
+        std::getline(inputFile, instructionLine);
+        parseInstructionLine(instructionLine, &ascension, &basementPositionIndex);
+
+        if (basementInstructionPosition < 0)
+        {
+            basementInstructionPosition = basementPositionIndex + 1;
+        }
+    }
+
+    std::cout << "Puzzle 1: " << ascension << "\n";
+    std::cout << "Puzzle 2: " << basementInstructionPosition << "\n";
+}
