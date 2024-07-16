e0d2ec75e6aefb850899e38ee39d2ea0df64b3d6
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-07-16 12:54:23 -0400

    Puzzle 1: 74

--- AdventOfCode2015/Day1/Day1.cpp
+++ AdventOfCode2015/Day1/Day1.cpp
@@ -2,15 +2,43 @@
 #include <iostream>
 #include <string>
 
+#define PARENTHESIS_OPEN '('
+#define PARENTHESIS_COLSED ')'
+
+int parseInstructionLine(std::string pInstructionLine)
+{
+    int ascension = 0;
+
+    int nbInstructions = pInstructionLine.length();
+    for (int i = 0; i < nbInstructions; i++)
+    {
+        char instruction = pInstructionLine[i];
+
+        if (instruction == PARENTHESIS_OPEN)
+        {
+            ascension++;
+        }
+        else if (instruction == PARENTHESIS_COLSED)
+        {
+            ascension--;
+        }
+    }
+
+    return ascension;
+}
+
 int main(int argc, char* argv[])
 {
-    std::ifstream inputFile(argv[0]);
+    char* intputPath = argv[1];
+    std::ifstream inputFile(intputPath);
     std::string instructionLine;
+    int ascension = 0;
 
     while (inputFile.good())
     {
         std::getline(inputFile, instructionLine);
+        ascension += parseInstructionLine(instructionLine);
     }
 
-    std::cout << "Hello World!\n";
+    std::cout << "Puzzle 1: " << ascension << "\n";
 }
