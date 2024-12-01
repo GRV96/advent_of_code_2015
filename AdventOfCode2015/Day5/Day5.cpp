a2022445cc68da9b874bab079a103d1c533455a7
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-12-01 00:56:38 -0500

    Puzzle 2: 82 (wrong)

--- AdventOfCode2015/Day5/Day5.cpp
+++ AdventOfCode2015/Day5/Day5.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -15,7 +16,7 @@ bool isVowel(const char pSomeChar)
         || pSomeChar == 'u';
 }
 
-bool isStringNice(const std::string& pEvaluatedStr)
+bool isStringNicePuzzle1(const std::string& pEvaluatedStr)
 {
     std::string::const_iterator it = pEvaluatedStr.begin();
     std::string::const_iterator endIt = pEvaluatedStr.end();
@@ -61,23 +62,57 @@ bool isStringNice(const std::string& pEvaluatedStr)
     return nbVowels >= 3 && twiceInARow;
 }
 
+bool isStringNicePuzzle2(const std::string& pEvaluatedStr)
+{
+    std::set<std::string> letterPairs;
+    bool wasIdenticalPairFound = false;
+    bool wasTrioFound = false;
+
+    int strLength = pEvaluatedStr.length();
+    int pairIndexBound = strLength - 2;
+    int trioIndexBound = strLength - 3;
+    for (int i = 0; i < strLength; i++)
+    {
+        if (i <= pairIndexBound && !wasIdenticalPairFound)
+        {
+            // If the set already contains the value, second is false.
+            std::string letterPair = pEvaluatedStr.substr(i, 2);
+            wasIdenticalPairFound = !letterPairs.insert(letterPair).second;
+        }
+
+        if (i <= trioIndexBound && !wasTrioFound)
+        {
+            wasTrioFound = pEvaluatedStr.at(i) == pEvaluatedStr.at(i + 2);
+        }
+    }
+
+    return wasIdenticalPairFound && wasTrioFound;
+}
+
 int main(int argc, char* argv[])
 {
     char* intputPath = argv[1];
     std::ifstream inputFile(intputPath);
     std::string inputLine;
 
-    int nbNiceStrings = 0;
+    int nbNiceStringsPuzzle1 = 0;
+    int nbNiceStringsPuzzle2 = 0;
     while (inputFile.good())
     {
         std::getline(inputFile, inputLine);
 
-        if (isStringNice(inputLine))
+        if (isStringNicePuzzle1(inputLine))
         {
-            nbNiceStrings++;
+            nbNiceStringsPuzzle1++;
+        }
+
+        if (isStringNicePuzzle2(inputLine))
+        {
+            nbNiceStringsPuzzle2++;
         }
     }
 
-    std::cout << "Puzzle 1: " << nbNiceStrings << std::endl;
+    std::cout << "Puzzle 1: " << nbNiceStringsPuzzle1 << std::endl;
+    std::cout << "Puzzle 2: " << nbNiceStringsPuzzle2 << std::endl;
     return 0;
 }
