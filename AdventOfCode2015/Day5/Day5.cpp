15bd5cc73f00e85d0e4796a70275ed9b28cbc8a9
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-11-30 22:12:27 -0500

    Puzzle 1: 238

--- AdventOfCode2015/Day5/Day5.cpp
+++ AdventOfCode2015/Day5/Day5.cpp
@@ -1,6 +1,63 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
+
+const std::vector<std::string> illegalSubstrings{ "ab", "cd", "pq", "xy" };
+
+bool isVowel(const char pSomeChar)
+{
+    return pSomeChar == 'a'
+        || pSomeChar == 'e'
+        || pSomeChar == 'i'
+        || pSomeChar == 'o'
+        || pSomeChar == 'u';
+}
+
+bool isStringNice(const std::string& pEvaluatedStr)
+{
+    std::string::const_iterator it = pEvaluatedStr.begin();
+    std::string::const_iterator endIt = pEvaluatedStr.end();
+
+    int nbVowels = 0;
+    bool twiceInARow = false;
+
+    while (true)
+    {
+        char letter = *it;
+
+        if (isVowel(letter))
+        {
+            nbVowels++;
+        }
+
+        std::string::const_iterator nextIt = it + 1;
+        if (nextIt == endIt)
+        {
+            break;
+        }
+        else
+        {
+            char nextLetter = *nextIt;
+
+            if (letter == nextLetter)
+            {
+                twiceInARow = true;
+            }
+
+            std::string substring{ letter, nextLetter };
+            if (std::find(illegalSubstrings.begin(), illegalSubstrings.end(), substring) != illegalSubstrings.end())
+            {
+                return false;
+            }
+
+            it = nextIt;
+        }
+    }
+
+    return nbVowels >= 3 && twiceInARow;
+}
 
 int main(int argc, char* argv[])
 {
@@ -8,8 +65,16 @@ int main(int argc, char* argv[])
     std::ifstream inputFile(intputPath);
     std::string inputLine;
 
+    int nbNiceStrings = 0;
     while (inputFile.good())
     {
         std::getline(inputFile, inputLine);
+
+        if (isStringNice(inputLine))
+        {
+            nbNiceStrings++;
+        }
     }
+
+    std::cout << "Puzzle 1: " << nbNiceStrings << std::endl;
 }
