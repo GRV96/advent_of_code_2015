9ed43899d49bacc54df111dd886044d9255a094c
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-04-05 20:39:07 -0400

    NOT FUNCTIONAL! Inexplicable errors. Headers have extension .hpp.

---
+++ AdventOfCode2015/DiscreteGrid/DiscreteGrid.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <map>
+
+#include "Coordinates.hpp"
+
+template <typename T>
+class DiscreteGrid
+{
+private:
+	std::map<Coordinates, T>* _grid;
+
+public:
+	DiscreteGrid();
+	нн~DiscreteGrid();
+
+	typename std::map<Coordinates, T>::iterator begin() const;
+	typename std::map<Coordinates, T>::iterator end() const;
+
+	bool get(const Coordinates& pCoordinates, T& pValue) const;
+
+	void set(const Coordinates& pCoordinates, const T& pValue);
+	void set(const Coordinates&& pCoordinates, const T&& pValue);
+};
